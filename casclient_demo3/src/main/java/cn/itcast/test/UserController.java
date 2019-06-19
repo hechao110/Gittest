37f794739389d1981fb3efbe29c5c8baee2ab3c0
hechao110 <842347212@qq.com>
2019-06-19 16:59:14 +0800

    spring-security-cas

--- casclient_demo3/src/main/java/cn/itcast/test/UserController.java
+++ casclient_demo3/src/main/java/cn/itcast/test/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class UserController {//000
     @RequestMapping("/findLoginUser")
     public void  findLoginUser(){
         String name =
